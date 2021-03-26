5f4d11c007b1987a530cede841375350d527cd06
Christopher <cyoun@chapman.edu>
2021-03-26 15:04:28 -0700

    create nav component

---
+++ frontend/src/components/Nav/Nav.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+
+
+export const Nav = ({children}) => {
+	return (
+		<nav className='flex'>
+			{children}
+		</nav>
+	)
+}
+
+
+export const NavLink = ({to, children}) => {
+	return (
+		<Link
+			className='nav__link'
+			style={{
+				margin: '0 1.40625em',
+			}}
+			to={to}
+		>
+			{children}
+		</Link>
+	)
+}
