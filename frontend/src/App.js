3359cd48f3b768ca30f0441a5ea4fe98d451c3b5
Christopher <cyoun@chapman.edu>
2021-03-26 15:03:31 -0700

    create mui theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,42 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
 import './assets/scss/main.scss'
 
 import Home from './pages/Home/Home'
 
 
+const customTheme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#97A885',
+			main: '#718858',
+			dark: '#62774C'
+		},
+		secondary: {
+			light: '#6D6D6D',
+			main: '#444444',
+			dark: '#000000'
+		},
+		grey: {
+			400: '#5e5e5e',
+			800: '#444444'
+		},
+	},
+	typography: {
+		fontFamily: '"Poppins",sans-serif'
+	},
+})
+
+
 const App = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path='/' component={Home} />
-			</Switch>
-		</Router>
+		<ThemeProvider theme={customTheme}>
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Home} />
+				</Switch>
+			</Router>
+		</ThemeProvider>
 	)
 }
 
