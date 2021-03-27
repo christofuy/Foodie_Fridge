7cb4468ee66622d999ae831742ce39bcb63d6c3f
Mitch Radvansky <mitch.radman101@gmail.com>
2021-03-26 19:55:01 -0700

    deleted original server js file

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,98 @@
-const express = require('express');
-const cors = require('cors');
+const dotenv = require("dotenv");
+dotenv.config();
+var express = require('express');
+var app = express();
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true ,useUnifiedTopology: true});
 
-const app = express();
-const port = process.env.PORT || 5000;
+//creates User schema
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name : {type : String, required : true},
+    email : {type : String, required : true},
+    password : {type : String, min: 8, required : true} 
+})
 
-//Middlware
-app.use(cors({origin: 'http://localhost:3000'})); //allow cors
-app.use(express.json());
+const User = mongoose.model('User',userSchema);
 
-//const uri = process.atlas.env.ATLAS_URI;
-//mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true}
-//);
-//const connection = mongoose.connection;
-//connection.once('open', () => {
-//console.log("MongoDB database connection established successfully");
-//})
+const port = 3000;
 
+app.listen(port, () => console.log("listening on port " + port))
 
-//Add routes to API
-app.use('/api/chat', require('./routes/requests'))
-
-app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+//creates user
+router.post('/:id/create', (req,res) => {
+    const {name, email, password} = req.body
+    if (!name || !email || !password) {
+        return res.status(400).json({msg: 'Please input in all boxes'})
+    }
+    const newUser = new User({name,email,password})
+    bcrypt.getSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+            newUser.save((err, user) => {
+                if (err) return res.json({err})
+                req.session.userId = user.id
+            })
+        })
+    })
 });
+
+//reads user credentials and renders it
+router.get('/:id', (req,res) => {
+    res.send(req.params.id);
+});
+
+router.get('/',(req,res,next)=>{
+   res.send('Test'); 
+});
+
+//updates user
+router.put('/:id/delete', (req,res) => {
+    var id = req.params.id;
+    User.findOne({_id : id}, function(err, foundObject) {
+        if (err) {
+            console.log(err);
+            res.status(500).send();
+        } else {
+            if(!foundObject) {
+                res.status(404).send();
+            } else {
+                if(req.body.name) {
+                    foundObject.name = req.body.name;
+                }
+                if(req.body.email) {
+                    foundObject.email = req.body.email;
+                }
+                if(req.body.password) {
+                    foundObject.password = req.body.password;
+                }
+
+                foundObject.save(function(err, updatedObject) {
+                    if(err) {
+                        console.log(err);
+                        res.status(500).send();
+                    }else{
+                        res.send(updatedObject);
+                    }
+                });
+            }
+        }
+    });
+});
+
+//deletes user
+router.delete('/api/user/:id', (req,res) => {
+    User.findByIdandRemove(req.params.id).exec()
+    .then(doc => {
+        if (!doc) { return res.status(404).end();}
+        return res.status(204).end();
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router
