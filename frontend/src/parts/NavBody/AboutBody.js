bef0dcc36aed9586331c258966f8b4223c003c05
Nick <nick_kim00@hotmail.com>
2021-03-27 13:32:00 -0700

    Made about us page

--- frontend/src/parts/NavBody/AboutBody.js
+++ frontend/src/parts/NavBody/AboutBody.js
@@ -1,9 +1,33 @@
+import './aboutbody.scss'
+
 const AboutBody = () => {
 	return (
 		<div>
-			<p>Foodie Fridge was created after we began to notice the amount of waste from home cooking, even if accidentally. 
-                Maybe it would be the bag of vegetables on the bottom of your refrigerator, or some old pasta you forgot to reheat.
+			<p className = 'about-intro'>
+				Foodie Fridge was created after we began to notice the amount of waste that came from our home cooking, most of it accidental. 
+                Maybe it would be the lost bag of vegetables on the bottom of your refrigerator, or some leftover pasta you forgot about.
+				No matter where it came from, we felt that it would be the least we can do to try and be more mindful of the food that we 
+				have, and not let it go to waste. 
             </p>
+
+			<p className = 'about-text'>
+				The United States Department of Agriculture estimates that about 30-40% of the food supply goes to waste
+				<sup>
+					<a href = 'https://www.fda.gov/food/consumers/food-loss-and-waste#:~:text=In%20the%20United%20States%2C%20food,worth%20of%20food%20in%202010.'>[1]</a>
+				</sup>, 
+				a number much too high when considering that poverty, homelessness, and starvation still exists in the world. Whether you are a home chef or a professional, 
+				we hope that our service can help reduce waste without the overhead of expensive equipment or inventory management systems.
+            </p>
+
+			<p className = 'about-support'>
+				-Christopher, Mitch, and Nicholas
+			</p>
+
+			<p className = 'about-cite'>
+				<a href = 'https://www.fda.gov/food/consumers/food-loss-and-waste#:~:text=In%20the%20United%20States%2C%20food,worth%20of%20food%20in%202010.'>
+				<sup>[1]</sup> "Food Loss and Waste". U.S. Food & Drug Administration, 23 February 2021.
+				</a>
+			</p>
 		</div>
 	)
 }
--- frontend/src/parts/NavBody/ServicesBody.js
+++ frontend/src/parts/NavBody/ServicesBody.js
@@ -1,7 +1,8 @@
 const ServicesBody = () => {
 	return (
 		<div>
-			<p>Foodie Fridge provides users.
+			<p>Foodie Fridge provides users with the ability to keep track of food they currently have, and how close it would be to expiring. In the future,
+				we foresee further implementation with hardware and image recognition to make it easier for users.
             </p>
 		</div>
 	)
