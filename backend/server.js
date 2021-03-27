67c9030dd6299339ac7c684aa77d4a214bbb5b43
Christopher <cyoun@chapman.edu>
2021-03-26 21:13:14 -0700

    refactor

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
+const express = require('express')
 const dotenv = require("dotenv");
-dotenv.config();
 const cors = require('cors')
-var express = require('express');
-var app = express();
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true ,useUnifiedTopology: true});
 
-app.use(cors({origin: 'http://localhost:3000'}));
+const app = express()
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("listening on port " + port))
+
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(cors({origin: 'http://localhost:3000'}));
+
 
 app.use(express.json());
 app.use('/api/user', require('./routes/users_route'));
 
-module.exports = router
+
+app.listen(port, () => console.log("listening on port " + port))
