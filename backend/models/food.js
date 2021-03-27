b1fc62af4b60299b1ae23fe718bc217c4482a717
Mitch Radvansky <mitch.radman101@gmail.com>
2021-03-27 12:53:18 -0700

    created food model and setup for food route

--- backend/models/food.js
+++ backend/models/food.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+//schema for fridge items
 const Schema = mongoose.Schema;
-const foodSchema = Schema({
-    
-})
+const foodSchema = 
+    db.fridges.aggregate([
+        { $fridge: { items : { $concatArrays:["expired","non-expired"]}}}
+    ])
 
 const Food = mogoose.model('Food',foodSchema);
 
