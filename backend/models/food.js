482c7191086f216e894fdc3646f29c84b2600446
Mitch Radvansky <mitch.radman101@gmail.com>
2021-03-26 21:32:21 -0700

    setup for food model

---
+++ backend/models/food.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const foodSchema = Schema({
+    
+})
+
+const Food = mogoose.model('Food',foodSchema);
+
+module.exports = Food;
