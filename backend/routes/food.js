5113c552132eb9edc12bf1eac1d20238fa1ea78d
Mitch Radvansky <mitch.radman101@gmail.com>
2021-03-27 15:13:37 -0700

    added post method to add food to fridge

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -5,6 +5,9 @@ let Food = require('../models/food.model.js')
 
 //adds food to fridge
 router.post('/add', (req,res) => {
-    const{item} = req.body;
-    
+    const{item, date} = req.body;
+    if (!items || !date) {
+        return res.status(400).json({msg: 'Please input in both boxes'})
+    }
+    const newItems = new Food({item, date})
 });
