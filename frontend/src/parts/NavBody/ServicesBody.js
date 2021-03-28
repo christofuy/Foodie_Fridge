0c4eddbe291fb0700f56b75da920a65ebb1dda1a
Nick <nick_kim00@hotmail.com>
2021-03-27 22:40:29 -0700

    Modify services page

--- frontend/src/parts/NavBody/ServicesBody.js
+++ frontend/src/parts/NavBody/ServicesBody.js
@@ -9,9 +9,9 @@ const ServicesBody = () => {
             </p>
 
 			<p className = 'services-wip'>
-				A service in progress is the addition of image recognition that would make it easier for users to keep track of food they have, as well as their 
+				A very useful feature that may be next is the addition of image recognition, which would make it easier for users to keep track of food they have, as well as their 
 				expiration dates. With the aid of a user's phone or camera, we would be able to process and categorize foods quickly and accurately. In addition,
-				we would continue to (as always!) try our best to improve the user experience.
+				we will continue to (as always!) try our best to improve the user experience.
 			</p>
 		</div>
 	)
